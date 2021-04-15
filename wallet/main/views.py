c2b73609290bf0d4c56b0432d386ce43d1ac84f2
Nikita Ivanov <ivnnkt@gmail.com>
2021-04-15 22:17:08 +0300

    добавил страницы просмотра списка кошельков и каждого по отдельности, возможность редактировать и удалять кошельки, страницу создания транзакций

--- wallet/main/views.py
+++ wallet/main/views.py
@@ -1,16 +1,76 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Wallet, Transactions
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import WalletForm, TransactionsForm
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ValidationError
 
 
-def index(request):
-    """Главная страница. Выводится список товаров,
-    цена если turn_on_block = True, и пример работы фильтра revers_string.
+class WalletListView(LoginRequiredMixin, generic.ListView):
+    """Полный список кошельков.
     """
-    # prod = Product.objects.all()
+    model = Wallet
+    template_name = 'main/wallets.html'
 
-    return render(
-        request,
-        'main/index.html',
-        {
-            # 'prod': prod,
-        }
-    )
+
+class WalletDetailView(LoginRequiredMixin, generic.DetailView):
+    """Страница кошелька
+    """
+    model = Wallet
+    template_name = 'main/wallet.html'
+
+
+class WalletCreateView(LoginRequiredMixin, generic.CreateView):
+    """Страница добавления кошелька
+    """
+    model = Wallet
+    form_class = WalletForm
+    success_url = '/wallets'
+
+
+class WalletUpdaitView(LoginRequiredMixin, generic.UpdateView):
+    """Страница редактирования кошелька
+    """
+    model = Wallet
+    form_class = WalletForm
+    template_name = 'main/wallet_edit.html'
+    success_url = '/wallets'
+
+
+class WalletDeleteView(LoginRequiredMixin, generic.DeleteView):
+    """Страница удаления кошелька
+    """
+    model = Wallet
+    template_name = 'main/wallet_del.html'
+    success_url = '/wallets'
+
+
+class TransactionListView(LoginRequiredMixin, generic.ListView):
+    """Страница истории всех транзакций
+    """
+    model = Transactions
+    template_name = 'main/transactions.html'
+
+
+class TransactionCreateView(LoginRequiredMixin, generic.CreateView):
+    """Страница создания транзакции
+    """
+    model = Transactions
+    form_class = TransactionsForm
+    success_url = '/wallets'
+
+    def form_valid(self, form):
+        obj = form.instance
+        wallet = Wallet.objects.get(name=obj.wallet)
+        if obj.transaction_type=='plus':
+            wallet.balance += obj.amount
+            wallet.save()
+            self.object = form.save()
+        else:
+            if obj.amount > wallet.balance:
+                raise ValidationError('На счете кошелька недостаточно средств!')
+            else:
+                wallet.balance -= obj.amount
+                wallet.save()
+                self.object = form.save()
+        return HttpResponseRedirect('/wallets')
